37974348ba20e6173fb8cf17ec55d493932bee39
David Howell <david@davidjameshowell.com>
2021-08-03 10:33:30 -0700

    Update docker-entrypoint.sh

--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ash
 
-wget https://github.com/cloudflare/cloudflared/releases/download/2021.8.0/cloudflared-linux-arm64 -O /tmp/cloudflared
+wget https://github.com/cloudflare/cloudflared/releases/download/2021.8.0/cloudflared-linux-arm -O /tmp/cloudflared
 chmod +x /tmp/cloudflared
 /commento/commento &
 /tmp/cloudflared tunnel --config=/root/.cloudflared/${CLOUDFLARED_CONFIG} run ${CLOUDFLARED_TUNNEL} 
